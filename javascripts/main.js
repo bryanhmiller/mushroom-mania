fa8b36503de606cbd2b6fcd2f99c2fe6d06c3543
bryanhmiller <bryankalos@hotmail.com>
2017-05-14 18:39:24 -0500

    Continued working on page display, incomplete, also worked on retrieving data from firebase, unsuccessful, NavCtrl error

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,40 @@
-app.run((FIREBASE_CONFIG) =>{
-  firebase.initializeApp(FIREBASE_CONFIG);
+app.run((FIREBASE_CONFIG) => {
+  	firebase.initializeApp(FIREBASE_CONFIG);
 });
 
 app.controller("PickerCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 
 	$scope.cat = "Meow";
 	$scope.navItems = [{name : "Logout"}, {name : "All Items"}, {name : "New Item"}];
+
+	let getMushroomList = () => {
+		let mushroomz = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+			.then((fbMushroom) => {
+				let mushroomCollection = fbMushroom.data;
+				Object.keys(mushroomCollection).forEach((key) => {
+					mushroomCollection[key].id=key;
+					mushroomz.push(mushroomCollection[key]);
+			});
+				resolve(mushroomz);
+			}).catch((error) => {
+				reject(error);
+			});  
+		});  
+	};
+
+
+	let getMushrooms = () => {
+		getMushroomList()
+		.then((mushroomz) => {
+			$scope.mushrooms = mushroomz;
+			console.log("mushroomz", mushroomz);
+			console.log("$scope.mushrooms", $scope.mushrooms);
+		}).catch((error) => {
+			console.log("get Error", error);
+		});
+	};
+
+	getItems();
 });
