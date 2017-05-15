f12550b5a85f28275db2751e762368ded151b262
bryanhmiller <bryankalos@hotmail.com>
2017-05-15 16:05:33 -0500

    Completed xhr to firebase, now able to console.log data

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,7 @@ app.run((FIREBASE_CONFIG) => {
 
 app.controller("PickerCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 
-	$scope.cat = "Meow";
+	
 	$scope.navItems = [{name : "Logout"}, {name : "All Items"}, {name : "New Item"}];
 
 	let getMushroomList = () => {
@@ -36,5 +36,5 @@ app.controller("PickerCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 		});
 	};
 
-	getItems();
+	getMushrooms();
 });
